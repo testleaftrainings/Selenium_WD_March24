91fe954394333bec9e26985f9a5978ca041ad346
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2024-03-20 14:54:26 +0530

    Add files via upload

---
+++ week1/day1/FirstProgram.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class FirstProgram {
+
+	//main -->Press ctrl+space bar
+	public static void main(String[] args) {
+		//syso press ctrl+space bar
+		System.out.println("My First Program");
+	}
+	public static void main1(String[] args) {
+		//syso press ctrl+space bar
+		System.out.println("Hellow");
+	}
+}
