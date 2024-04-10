7c59a357eead9ceb7ad230a315bbce1fbfd95149
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2024-04-10 13:33:29 +0530

    Add files via upload

---
+++ week4/testcases/LearnTest.java
@@ -0,0 +1,25 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class LearnTest {
+
+	@Test
+	public void addition() {
+		System.out.println("Addition");
+	}
+	@Test
+	public void mul() {
+		System.out.println("Multiplication");
+	}
+	@Test
+	public void div() {
+		System.out.println("Division");
+	}
+	@Test
+	public void sub() {
+		System.out.println("Subraction");
+	}
+	
+	
+}
